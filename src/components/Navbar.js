47e909fa074c329713aed9ab607b7d38a53981ea
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-08-20 22:56:55 -0500

    added color vars for nav bar/ rudimentary styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,30 @@
 import React from "react";
 import Bus from "../icons/cuteBus.png";
+import "./Navbar.scss";
 
 export default function Navbar() {
   return (
     <div className="navContainer">
-      <div className="leftSideNav">
-        <div className="logoContainer">
-          <img src={Bus} alt="School Bus" />
+      <div className="navDiv">
+        <div className="leftSideNav">
+          <div className="logoContainer">
+            <img src={Bus} alt="School Bus" />
+          </div>
+          <div className="title">
+            <h1>Field Trip Planner</h1>
+          </div>
         </div>
-        <div className="title">
-          <h1>Field Trip Planner</h1>
+        <div className="rightSideNav">
+          <div className="navLinks">
+            <a>How it works</a>
+            <a>Learn More</a>
+          </div>
+          <div className="onboardingLinks">
+            <a href="#">Log in</a>
+            <a href="#">Sign up</a>
+          </div>
         </div>
       </div>
-      <div className="rightSideNav"></div>
     </div>
   );
 }
