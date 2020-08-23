7781ddc3edc2c7d4ecc97f8a6e4e234e35e50084
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-08-23 18:18:08 -0500

    reorganized file structure for videoplayer componenents

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.scss";
 
 import Navbar from "./components/Navigation/Navbar.js";
-import VideoPlayer from "./components/MarketingContent/VideoPlayer";
+import VideoPlayer from "./components/MarketingContent/LandingContent/VideoPlayer/VideoPlayer.js";
 
 function App() {
   return (
--- src/components/MarketingContent/LandingContent/VideoPlayer/VideoPlayer.js
+++ src/components/MarketingContent/LandingContent/VideoPlayer/VideoPlayer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactPlayer from "react-player/youtube";
-import "./LandingContent/VideoPlayer.scss";
+import "./VideoPlayer.scss";
 
 export default function LandingDiv() {
   return (
@@ -9,8 +9,8 @@ export default function LandingDiv() {
         className="react-player"
         light={false}
         url="https://www.youtube.com/watch?v=EWPFmdAWRZ0"
-        width="50%"
-        height="50%"
+        width="35%"
+        height="35%"
         style={{
           minHeight: "200px",
           minWidth: "300px",
