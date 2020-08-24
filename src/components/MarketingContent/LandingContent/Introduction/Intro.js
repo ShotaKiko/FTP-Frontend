82f988b13a5fd43c834ef60f215fc550f443b1f8
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-08-24 16:42:10 -0500

    built out landing layout and added styling

--- src/components/MarketingContent/LandingContent/Introduction/Intro.js
+++ src/components/MarketingContent/LandingContent/Introduction/Intro.js
@@ -1,6 +1,9 @@
 import React from "react";
+import "./Intro.scss";
+
 import VideoPlayer from "../VideoPlayer/VideoPlayer.js";
 import {
+  Jumbotron,
   Card,
   CardImg,
   CardText,
@@ -14,28 +17,19 @@ function Intro() {
   return (
     <div className="intro-container">
       <div className="leftSide">
-        <div className="messageDiv">
-          <h2>Make every field trip as easy as A, B, C</h2>
-          <h5>One simple to use app for all your field trip needs!</h5>
+        <Jumbotron className="jumbotron">
+          <h2 className="display-4">
+            Make planning every field trip<br></br> as easy as A, B, C
+          </h2>
+          <p className="lead">
+            One simple to use app for all your field trip needs!{" "}
+          </p>
+          <hr className="my-2" />
           <p>Sign up as a...</p>
-        </div>
-        <div className="signUpOptions">
-          <Card>
-            <CardTitle>Teacher</CardTitle>
-          </Card>
-          <Card>
-            <CardTitle>Parent</CardTitle>
-          </Card>
-          <Card>
-            <CardTitle>Administrator</CardTitle>
-          </Card>
-          <Card>
-            <CardTitle>Student</CardTitle>
-          </Card>
-          <Card>
-            <CardTitle>Bus Driver</CardTitle>
-          </Card>
-        </div>
+          <p className="lead">
+            <Button color="primary">Learn More</Button>
+          </p>
+        </Jumbotron>
       </div>
 
       <div className="rightSide">
