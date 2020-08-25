7d897ce0b08c2a5915d8aa32e28e06d8cb327a1e
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-08-24 22:49:02 -0500

    added sign up buttons and corresponding icons

--- src/components/MarketingContent/LandingContent/Introduction/Intro.js
+++ src/components/MarketingContent/LandingContent/Introduction/Intro.js
@@ -2,16 +2,13 @@ import React from "react";
 import "./Intro.scss";
 
 import VideoPlayer from "../VideoPlayer/VideoPlayer.js";
-import {
-  Jumbotron,
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-  Button,
-} from "reactstrap";
+import { Jumbotron, Button } from "reactstrap";
+
+import Admin from "../../../../icons/administrator.svg";
+import Teacher from "../../../../icons/teacherDesk.svg";
+import Parent from "../../../../icons/parentHouse.svg";
+import Student from "../../../../icons/studentBackpack.svg";
+import SchoolBus from "../../../../icons/schoolBus.svg";
 
 function Intro() {
   return (
@@ -19,15 +16,34 @@ function Intro() {
       <div className="leftSide">
         <Jumbotron className="jumbotron">
           <h2 className="display-4">
-            Make planning every field trip<br></br> as easy as A, B, C
+            Make planning every field<br></br>trip as easy as A, B, C
           </h2>
           <p className="lead">
             One simple to use app for all your field trip needs!{" "}
           </p>
           <hr className="my-2" />
           <p>Sign up as a...</p>
-          <p className="lead">
-            <Button color="primary">Learn More</Button>
+          <p className="options">
+            <Button className="signInOptionButton" outline color="primary">
+              <img src={Teacher} alt="Teacher Desk" />
+              Teacher
+            </Button>
+            <Button className="signInOptionButton" outline color="primary">
+              <img src={Parent} alt="Parent's home" />
+              Parent
+            </Button>
+            <Button className="signInOptionButton" outline color="primary">
+              <img src={Student} alt="Student Backpack" />
+              Student
+            </Button>
+            <Button className="signInOptionButton" outline color="primary">
+              <img src={Admin} alt="Admin Badge" />
+              Administrator
+            </Button>
+            <Button className="signInOptionButton" outline color="primary">
+              <img src={SchoolBus} alt="School Bus" />
+              Bus Driver
+            </Button>
           </p>
         </Jumbotron>
       </div>
